b85f73f5964d120d4a9feb6a3bfbe8b79e5c925e
santiago <santiagograjales0610@gmail.com>
2024-04-27 11:56:09 -0500

    ejercicio1semana3

---
+++ Cohorte II/parcial2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import matplotlib as plt
+import numpy as np
+
+df1 = pd.read_csv("C:\winequality-red.csv")
+df2 = pd.read_csv("C:\winequality-white.csv")
+
+df.head()
+
+missing_data = df.isnull()
+missing_data.head()
+
+
+
+
+
+
